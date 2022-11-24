16f1b65e17a2ebf0cf5f4481a570dd27653ac9fd
website <47500356+Ibrahim012khan@users.noreply.github.com>
2022-11-24 20:21:52 +0600

    Remove image attribute from Divi on Hover

--- divi_script.js
+++ divi_script.js
@@ -3,3 +3,18 @@
 <script type="text/javascript">
 var elm=document.getElementsByTagName("html")[0];elm.style.display="none";document.addEventListener("DOMContentLoaded",function(event) {elm.style.display="block"; });
 </script>
+
+/** Remove Image Attribute Text on Hove **/
+<script>
+jQuery(document).ready(function($) {
+    $("img").mouseenter(function() {
+        let $lwp_title = $(this).attr("title");
+        $(this).attr("lwp_title", $lwp_title);
+        $(this).attr("title", "");
+    }).mouseleave(function() {
+        let $lwp_title = $(this).attr("lwp_title");
+        $(this).attr("title", $lwp_title);
+        $(this).removeAttr("lwp_title");
+    });
+});
+</script>
