c1549ca6dac5617f8beb5b11d1c775672071b837
Ibrahim Khan <ibrahim012khan@gmail.com>
2023-08-23 20:22:52 +0600

    Conditional Child to Parent show or hide

---
+++ Custom.js
@@ -0,0 +1,6 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+$(document).ready(function() {
+  // Clild Sectors and selectors we are going to hide need to separate with comma
+  $('.child_selector').closest('.closest_parent_div').find('selectors needs to hide').hide();
+  $('.child_selector').closest('.closest_parent_div').find('selectors needs to hide').show();
+});	
